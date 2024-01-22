ed6b701606cc0e03355604e15c852f15220f5f20
mcallisterdavid <davidrmc2@gmail.com>
2024-01-22 22:48:24 +0000

    Updates

--- pds/pds.py
+++ pds/pds.py
@@ -55,10 +55,10 @@ class PDS(object):
         Computes an estimated posterior mean \mu_\phi(x_t, y; \epsilon_\phi).
         """
         device = self.device
-        beta_t = self.scheduler.betas[t].to(device)
-        alpha_t = self.scheduler.alphas[t].to(device)
-        alpha_bar_t = self.scheduler.alphas_cumprod[t].to(device)
-        alpha_bar_t_prev = self.scheduler.alphas_cumprod[t_prev].to(device)
+        beta_t = self.scheduler.betas[t].to(device)[:, None, None, None]
+        alpha_t = self.scheduler.alphas[t].to(device)[:, None, None, None]
+        alpha_bar_t = self.scheduler.alphas_cumprod[t].to(device)[:, None, None, None]
+        alpha_bar_t_prev = self.scheduler.alphas_cumprod[t_prev].to(device)[:, None, None, None]
 
         pred_x0 = (xt - torch.sqrt(1 - alpha_bar_t) * noise_pred) / torch.sqrt(alpha_bar_t)
         c0 = torch.sqrt(alpha_bar_t_prev) * beta_t / (1 - alpha_bar_t)
@@ -183,6 +183,131 @@ class PDS(object):
         grad = torch.nan_to_num(grad)
         target = (tgt_x0 - grad).detach()
         loss = 0.5 * F.mse_loss(tgt_x0, target, reduction=reduction) / batch_size
+        if return_dict:
+            dic = {"loss": loss, "grad": grad, "t": t}
+            return dic
+        else:
+            return loss
+        
+    def pred_for_x_hat(
+        self,
+        pred_original_sample: torch.FloatTensor,
+        t: int,
+        sample: torch.FloatTensor,
+        scheduler,
+    ):
+        device = sample.device
+        if scheduler.config.prediction_type == "v_prediction":
+            alpha_prod_t = scheduler.alphas_cumprod[t]
+            beta_prod_t = 1 - alpha_prod_t
+            v_pred = (sample * alpha_prod_t ** (0.5) - pred_original_sample) / (beta_prod_t ** (0.5))
+            pred = v_pred
+        elif scheduler.config.prediction_type == "epsilon":
+            alpha_prod_t = scheduler.alphas_cumprod[t].to(device)
+            alpha_prod_t = alpha_prod_t[:, None, None, None]
+            beta_prod_t = 1 - alpha_prod_t
+            v_pred = sample * alpha_prod_t - pred_original_sample
+            v_pred = (sample * alpha_prod_t ** (0.5) - pred_original_sample) / (beta_prod_t ** (0.5))
+            pred_epsilon = (alpha_prod_t**0.5) * v_pred + (beta_prod_t**0.5) * sample
+            pred = pred_epsilon
+        else:
+            raise ValueError()
+
+        return pred
+
+    def pds_gen(
+        self,
+        im,
+        prompt=None,
+        reduction="mean",
+        return_dict=False,
+    ):
+        device = self.device
+        scheduler = self.scheduler
+
+        # process text.
+        self.update_text_features(tgt_prompt=prompt)
+        tgt_text_embedding = self.tgt_text_feature
+        uncond_embedding = self.null_text_feature
+
+        batch_size = im.shape[0]
+        t, t_prev = self.pds_timestep_sampling(batch_size)
+        beta_t = scheduler.betas[t].to(device)
+        alpha_bar_t = scheduler.alphas_cumprod[t].to(device)
+        alpha_bar_t_prev = scheduler.alphas_cumprod[t_prev].to(device)
+        sigma_t = ((1 - alpha_bar_t_prev) / (1 - alpha_bar_t) * beta_t) ** (0.5)
+
+        noise = torch.randn_like(im)
+        noise_t_prev = torch.randn_like(im)
+
+        latents_noisy = scheduler.add_noise(im, noise, t)
+        latent_model_input = torch.cat([latents_noisy] * 2, dim=0)
+        text_embeddings = torch.cat([tgt_text_embedding, uncond_embedding] * batch_size, dim=0)
+
+        noise_pred = self.unet.forward(
+            latent_model_input,
+            torch.cat([t] * 2).to(device),
+            encoder_hidden_states=text_embeddings,
+        ).sample
+        noise_pred_text, noise_pred_uncond = noise_pred.chunk(2)
+        noise_pred = noise_pred_uncond + self.config.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+        x_t_prev = scheduler.add_noise(im, noise_t_prev, t_prev)
+
+        noise_pred_src = self.pred_for_x_hat(im, t, latents_noisy, scheduler)
+
+        mu_src = self.compute_posterior_mean(latents_noisy, noise_pred_src, t, t_prev)
+        mu_tgt = self.compute_posterior_mean(latents_noisy, noise_pred, t, t_prev)
+
+        zt_src = (x_t_prev - mu_src) / sigma_t[:, None, None, None]
+        zt_tgt = (x_t_prev - mu_tgt) / sigma_t[:, None, None, None]
+
+        grad = zt_tgt - zt_src
+        grad = torch.nan_to_num(grad)
+        target = (im - grad).detach()
+        loss = 0.5 * F.mse_loss(im, target, reduction=reduction) / batch_size
+        if return_dict:
+            dic = {"loss": loss, "grad": grad, "t": t}
+            return dic
+        else:
+            return loss
+        
+    def sds_loss(
+        self,
+        im,
+        prompt=None,
+        reduction="mean",
+        return_dict=False,
+    ):
+        device = self.device
+        scheduler = self.scheduler
+
+        # process text.
+        self.update_text_features(tgt_prompt=prompt)
+        tgt_text_embedding = self.tgt_text_feature
+        uncond_embedding = self.null_text_feature
+
+        batch_size = im.shape[0]
+        t, _ = self.pds_timestep_sampling(batch_size)
+
+        noise = torch.randn_like(im)
+
+        latents_noisy = scheduler.add_noise(im, noise, t)
+        latent_model_input = torch.cat([latents_noisy] * 2, dim=0)
+        text_embeddings = torch.cat([tgt_text_embedding, uncond_embedding], dim=0)
+        noise_pred = self.unet.forward(
+            latent_model_input,
+            torch.cat([t] * 2).to(device),
+            encoder_hidden_states=text_embeddings,
+        ).sample
+        noise_pred_text, noise_pred_uncond = noise_pred.chunk(2)
+        noise_pred = noise_pred_uncond + self.config.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+        w = 1 - scheduler.alphas_cumprod[t]
+        grad = w * (noise_pred - noise)
+        grad = torch.nan_to_num(grad)
+        target = (im - grad).detach()
+        loss = 0.5 * F.mse_loss(im, target, reduction=reduction) / batch_size
         if return_dict:
             dic = {"loss": loss, "grad": grad, "t": t}
             return dic
