78ef41cfaeaa982d8e86c36787a91ab83d11b272
mcallisterdavid <davidrmc2@gmail.com>
2024-01-24 20:44:32 +0000

    PDS + SDEdit

--- pds/pds.py
+++ pds/pds.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from diffusers import DDIMScheduler, DiffusionPipeline
 from jaxtyping import Float
 from PIL import Image
+import matplotlib.pyplot as plt
 
 
 @dataclass
@@ -272,6 +273,79 @@ class PDS(object):
         else:
             return loss
         
+
+    def pds_gen_sdedit_src(
+        self,
+        im,
+        prompt=None,
+        skip_percentage=0.5,
+        num_solve_steps=20,
+        reduction="mean",
+        return_dict=False,
+    ):
+        device = self.device
+        scheduler = self.scheduler
+
+        # process text.
+        self.update_text_features(tgt_prompt=prompt)
+        tgt_text_embedding = self.tgt_text_feature
+        src_text_embedding = self.tgt_text_feature
+        uncond_embedding = self.null_text_feature
+
+        tgt_x0 = im
+        src_x0 = self.run_sdedit(
+            x0=im,
+            tgt_prompt=prompt,
+            num_inference_steps=num_solve_steps,
+            skip=int(skip_percentage * num_solve_steps),
+        )
+
+        # Save debug image for SDEdit result
+        if torch.rand(1).item() > 0.7:
+            x0_decoded = self.decode_latent(src_x0).squeeze().permute(1, 2, 0).cpu().numpy()
+            plt.imsave('./sdedit_debug.png', x0_decoded)
+
+
+        batch_size = im.shape[0]
+        t, t_prev = self.pds_timestep_sampling(batch_size)
+        beta_t = scheduler.betas[t].to(device)
+        alpha_bar_t = scheduler.alphas_cumprod[t].to(device)
+        alpha_bar_t_prev = scheduler.alphas_cumprod[t_prev].to(device)
+        sigma_t = ((1 - alpha_bar_t_prev) / (1 - alpha_bar_t) * beta_t) ** (0.5)
+
+        noise = torch.randn_like(tgt_x0)
+        noise_t_prev = torch.randn_like(tgt_x0)
+
+        zts = dict()
+        for latent, cond_text_embedding, name in zip(
+            [tgt_x0, src_x0], [tgt_text_embedding, src_text_embedding], ["tgt", "src"]
+        ):
+            latents_noisy = scheduler.add_noise(latent, noise, t)
+            latent_model_input = torch.cat([latents_noisy] * 2, dim=0)
+            text_embeddings = torch.cat([cond_text_embedding, uncond_embedding], dim=0)
+            noise_pred = self.unet.forward(
+                latent_model_input,
+                torch.cat([t] * 2).to(device),
+                encoder_hidden_states=text_embeddings,
+            ).sample
+            noise_pred_text, noise_pred_uncond = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + self.config.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            x_t_prev = scheduler.add_noise(latent, noise_t_prev, t_prev)
+            mu = self.compute_posterior_mean(latents_noisy, noise_pred, t, t_prev)
+            zt = (x_t_prev - mu) / sigma_t
+            zts[name] = zt
+
+        grad = zts["tgt"] - zts["src"]
+        grad = torch.nan_to_num(grad)
+        target = (tgt_x0 - grad).detach()
+        loss = 0.5 * F.mse_loss(tgt_x0, target, reduction=reduction) / batch_size
+        if return_dict:
+            dic = {"loss": loss, "grad": grad, "t": t}
+            return dic
+        else:
+            return loss
+        
     def sds_loss(
         self,
         im,
@@ -341,7 +415,8 @@ class PDS(object):
                 encoder_hidden_states=text_embeddings,
             ).sample
             noise_pred_text, noise_pred_uncond = noise_pred.chunk(2)
-            noise_pred = noise_pred_uncond + self.config.guidance_scale * (noise_pred_text - noise_pred_uncond)
+            # noise_pred = noise_pred_uncond + self.config.guidance_scale * (noise_pred_text - noise_pred_uncond)
+            noise_pred = noise_pred_uncond + 7.5 * (noise_pred_text - noise_pred_uncond)
             xt = self.reverse_step(noise_pred, t, xt, eta=eta)
 
         return xt
