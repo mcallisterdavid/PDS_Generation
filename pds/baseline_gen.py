c56cff92d61135a61f372c7959d6ef6d586ce2be
Songwei Ge <gesongwei666@gmail.com>
2024-02-20 02:41:29 -0500

    baselines

--- pds/baseline_gen.py
+++ pds/baseline_gen.py
@@ -13,11 +13,10 @@ import mediapy
 import imageio
 from utils.imageutil import gaussian_blur, gaussian_kernel
 
-from diffusers import DDIMPipeline, DDIMScheduler, DDPMPipeline, DDPMScheduler, StableDiffusionPipeline
 from typing import *
 from jaxtyping import *
 
-from pds_exp import PDS, PDSConfig
+from guidance_baseline import Guidance, GuidanceConfig
 
 from datetime import datetime
 
@@ -32,9 +31,11 @@ def seed_everything(seed):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--prompt', type=str, default='a DSLR photo of a dog in a winter wonderland')
+parser.add_argument('--src_prompt', type=str, default='pixelated, foggy, hazy, blurry, noisy, malformed')
+parser.add_argument('--tgt_prompt', type=str, default='detailed, high resolution, high quality, sharp')
 parser.add_argument('--init_image_fn', type=str, default=None)
-parser.add_argument('--mode', type=str, default='pds', choices=['pds', 'sds', 'nfsd', 'pds_exp'])
-parser.add_argument('--src_method', type=str, default='step', choices=['sdedit', 'step', 'copy'])
+parser.add_argument('--mode', type=str, default='pds', choices=['pds', 'sds', 'nfsd', 'pds_exp', 'vsd'])
+parser.add_argument('--src_method', type=str, default='step', choices=['sdedit', 'step', 'copy', 'ddpm'])
 # parser.add_argument('--grad_method', type=str, default='z', choices=['eps', 'xps', 'x', 'z', 'iid_z', 'iid_eps', 'iid_xps', 'ood_z', 'ood_eps', 'ood_xps', 'condonly'])
 parser.add_argument('--grad_method', type=str, default='z')
 parser.add_argument('--cfg_scale', type=float, default=100)
@@ -48,9 +49,9 @@ args = parser.parse_args()
 
 init_image_fn = args.init_image_fn
 
-pds = PDS(PDSConfig(
+guidance = Guidance(GuidanceConfig(
     sd_pretrained_model_or_path='stabilityai/stable-diffusion-2-1-base'
-))
+), use_lora=args.mode == 'vsd')
 
 # Blur logic
 # kernel_size = 10
@@ -60,12 +61,12 @@ pds = PDS(PDSConfig(
 if init_image_fn is not None:
     reference = torch.tensor(plt.imread(init_image_fn))[..., :3]
     reference = reference.permute(2, 0, 1)[None, ...]
-    reference = reference.to(pds.unet.device)
+    reference = reference.to(guidance.unet.device)
 
-    reference_latent = pds.encode_image(reference)
+    reference_latent = guidance.encode_image(reference)
     im = reference_latent
 else:
-    im = torch.randn((1, 4, 64, 64), device=pds.unet.device)
+    im = torch.randn((1, 4, 64, 64), device=guidance.unet.device)
 
 src_method = args.src_method if 'pds' in args.mode else ''
 grad_method = args.grad_method if 'pds' in args.mode else ''
@@ -78,7 +79,7 @@ seed_everything(args.seed)
 def decode_latent(latent):
     latent = latent.detach().to(device)
     with torch.no_grad():
-        rgb = pds.decode_latent(latent)
+        rgb = guidance.decode_latent(latent)
     rgb = rgb.float().cpu().permute(0, 2, 3, 1)
     rgb = rgb.permute(1, 0, 2, 3)
     rgb = rgb.flatten(start_dim=1, end_dim=2)
@@ -90,13 +91,22 @@ batch_size = 1
 
 im.requires_grad_(True)
 im.retain_grad()
+# noise = torch.randn_like(im)
+noise = None
 
 im_optimizer = torch.optim.AdamW([im], lr=args.lr, betas=(0.9, 0.99), eps=1e-15)
+if args.mode == 'vsd':
+    lora_optimizer = torch.optim.AdamW(
+        [
+            {"params": guidance.unet_lora.parameters(), "lr": 3e-4},
+        ],
+        weight_decay=0,
+    )
+
 decodes = []
 decodes_src = []
 
 for step in tqdm(range(args.n_steps)):
-    im_optimizer.zero_grad()
 
     if init_image_fn is None:
         # A (generation)
@@ -105,51 +115,68 @@ for step in tqdm(range(args.n_steps)):
         # B (projection to manifold)
         skip_percentage = 0.97
 
-    with torch.no_grad():
-        pds.config.guidance_scale = args.guidance_scale
-        if args.mode == 'pds':
-            pds_dict = pds.pds_gen_sdedit_src(
-                im=im,
-                prompt=args.prompt,
-                src_method=src_method,
-                grad_method=grad_method,
-                skip_percentage = skip_percentage,
-                src_cfg_scale=args.cfg_scale,
-                tgt_cfg_scale=args.cfg_scale,
-                num_solve_steps = 12 + min(step // 200, 20),
-                return_dict=True
-            )
-        elif args.mode == 'sds':
-            pds_dict = pds.sds_loss(
-                im=im,
-                prompt=args.prompt,
-                cfg_scale=args.cfg_scale,
-                return_dict=True
-            )
-        elif args.mode == 'nfsd':
-            pds_dict = pds.nfsd_loss(
-                im=im,
-                prompt=args.prompt,
-                cfg_scale=args.cfg_scale,
-                return_dict=True
-            )
-        elif args.mode == 'pds_exp':
-            pds_dict = pds.pds_gen_experimental(
-                im=im,
-                prompt=args.prompt,
-                src_method=src_method,
-                skip_percentage = skip_percentage,
-                src_cfg_scale=args.cfg_scale,
-                tgt_cfg_scale=args.cfg_scale,
-                num_solve_steps = 12 + min(step // 200, 20),
-                return_dict=True
-            )
+    guidance.config.guidance_scale = args.guidance_scale
+    if args.mode == 'pds':
+        pds_dict = guidance.pds_gen_sdedit_src(
+            im=im,
+            prompt=args.prompt,
+            src_method=src_method,
+            grad_method=grad_method,
+            src_prompt=args.src_prompt,
+            tgt_prompt=args.tgt_prompt,
+            skip_percentage = skip_percentage,
+            src_cfg_scale=args.cfg_scale,
+            tgt_cfg_scale=args.cfg_scale,
+            num_solve_steps = 12 + min(step // 200, 20),
+            noise=noise,
+            return_dict=True
+        )
+    elif args.mode == 'sds':
+        pds_dict = guidance.sds_loss(
+            im=im,
+            prompt=args.prompt,
+            cfg_scale=args.cfg_scale,
+            noise=noise,
+            return_dict=True
+        )
+    elif args.mode == 'nfsd':
+        pds_dict = guidance.nfsd_loss(
+            im=im,
+            prompt=args.prompt,
+            cfg_scale=args.cfg_scale,
+            return_dict=True
+        )
+    elif args.mode == 'pds_exp':
+        pds_dict = guidance.pds_gen_experimental(
+            im=im,
+            prompt=args.prompt,
+            src_method=src_method,
+            skip_percentage = skip_percentage,
+            src_cfg_scale=args.cfg_scale,
+            tgt_cfg_scale=args.cfg_scale,
+            num_solve_steps = 12 + min(step // 200, 20),
+            return_dict=True
+        )
+    elif args.mode == 'vsd':
+        pds_dict = guidance.vsd_loss(
+            im=im,
+            prompt=args.prompt,
+            cfg_scale=7.5,
+            return_dict=True
+        )
+        lora_loss = pds_dict['lora_loss']
+        lora_loss.backward()
+        lora_optimizer.step()
+        lora_optimizer.zero_grad()
+
+    # import ipdb; ipdb.set_trace()
     grad = pds_dict['grad']
     src_x0 = pds_dict['src_x0'] if 'src_x0' in pds_dict else grad
 
     # loss.backward()
     im.backward(gradient=grad)
     im_optimizer.step()
+    im_optimizer.zero_grad()
 
     if step % 10 == 0:
         decoded = decode_latent(im.detach()).cpu().numpy()
